59b22ce522a29ced5b3d0e5655dc33f8e81df2de
Romeo Van Snick <romeo@romeovansnick.be>
2025-04-21 22:30:34 +0200

    Remove unused helpers

--- frontend/src/metabase-lib/v1/expressions/utils.ts
+++ frontend/src/metabase-lib/v1/expressions/utils.ts
@@ -2,8 +2,6 @@ import * as Lib from "metabase-lib";
 import type Database from "metabase-lib/v1/metadata/Database";
 import type Metadata from "metabase-lib/v1/metadata/Metadata";
 
-import type { Node, Token } from "./pratt";
-
 export function getDatabase(
   query: Lib.Query,
   metadata?: Metadata,
@@ -22,22 +20,6 @@ export function getExpressionMode(startRule: string): Lib.ExpressionMode {
       return "filter";
   }
   throw new Error(`Unknown start rule: ${startRule}`);
-}
-
-type Nodable =
-  | unknown[]
-  | Lib.ExpressionParts
-  | Lib.ExpressionArg
-  | { node?: Node };
-
-export function getNode(item?: Nodable): Node | undefined {
-  if (typeof item === "object" && item !== null && "node" in item) {
-    return item?.node;
-  }
-}
-
-export function getToken(x?: Nodable): Token | undefined {
-  return getNode(x)?.token ?? undefined;
 }
 
 export function maybe<T extends { errors: Error[] }>(
